c6ff11a45e137014699c719e1c21668d3c21a3f5
Aman Rawat <amanrawat.js@gmail.com>
2025-09-01 22:07:20 +0530

    updates

--- blog/settings.py
+++ blog/settings.py
@@ -24,7 +24,7 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 SECRET_KEY = 'django-insecure-7tna-lukv1o)(hlh=!k4ue%6u@zl2x6c9^7&4zxvgg$*(8vo%6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
