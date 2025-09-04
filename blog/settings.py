69b142fe9edb313de14182607c9864c0a4242189
Aman Rawat <amanrawat.js@gmail.com>
2025-09-04 18:29:10 +0530

    changes

--- blog/settings.py
+++ blog/settings.py
@@ -12,12 +12,12 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from django.contrib.messages import constants as messages
 from pathlib import Path
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(BASE_DIR / ".env")   # <-- loads your .env
+# load_dotenv(BASE_DIR / ".env")  
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
